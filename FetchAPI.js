e6a1c30fcf7edead60eb4f47f050b65522344a9b
moises <moises.santos.oliveira91@gmail.com>
2020-05-03 20:06:20 -0300

    Pokemon pictures and stats display added with success

--- FetchAPI.js
+++ FetchAPI.js
@@ -10,14 +10,34 @@ getData = () =>{
     showData = (result) =>{
            for(const fields in result.results){
                let pokeName = result.results[fields].name;
-
-                let para = document.createElement('p');
-                let node = document.createTextNode(`${pokeName}`);
-                para.appendChild(node);
-                let div = document.getElementById('div1');
-                div.appendChild(para);
+               showStatus(pokeName);
             }
-        } 
+        }
+    showStatus = (name) =>{
+        fetch(`https://pokeapi.co/api/v2/pokemon/${name}`,operations).
+    then( (res) => res.json()
+    .then(data => {
+        console.log(data.sprites.back_default);
+        let div = document.getElementById('div1');
+        let divPoke = document.createElement('div');
+        let h1Name = document.createElement('h2');
+        let h2HP = document.createElement('h2');
+        let h2Type = document.createElement('h2');
+        h2Type.innerHTML = `Type: ${data.types[0].type.name}`;
+        h2HP.innerHTML = `HP: ${data.stats[5].base_stat}`;
+        h1Name.id = 'h1Name';
+        divPoke.id = 'divPoke';
+        h1Name.innerHTML = data.name;
+        let img = document.createElement('img');
+        img.src = data.sprites.back_default;
+        div.appendChild(divPoke);
+        divPoke.appendChild(img);
+        divPoke.appendChild(h1Name);
+        divPoke.appendChild(h2HP);
+        divPoke.appendChild(h2Type);
+    }))
+    
+    } 
     }
 
 getData();
